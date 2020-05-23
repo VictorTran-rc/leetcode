aff54357bb31d42b048d1cb630d17add3c5c5f68
Victor Tran <60512497+VictorTran-rc@users.noreply.github.com>
2020-05-22 21:55:52 -0400

    Add files via upload

---
+++ LeetCode/FindCommonCharacters.js
@@ -0,0 +1,37 @@
+Find Common Characters
+
+Given an array A of strings made only from lowercase letters, return a list of all characters that show up in all strings within the list (including duplicates).  For example, if a character occurs 3 times in all strings but not 4 times, you need to include that character three times in the final answer.
+
+You may return the answer in any order.
+
+Example 1:
+
+Input: ["bella","label","roller"]
+Output: ["e","l","l"]
+Example 2:
+
+Input: ["cool","lock","cook"]
+Output: ["c","o"]
+
+
+Note:
+
+1 <= A.length <= 100
+1 <= A[i].length <= 100
+A[i][j] is a lowercase letter
+
+
+var commonChars = function(A) {
+  return A.reduce((acc, next) => {
+    var array = [];
+    for (var i = 0; i < next.length; i ++) {
+      var key = next[i];
+      var index = acc.indexOf(key);
+      if (index > -1) {
+        acc.splice(index, 1)
+        array.push(key)
+      }
+    }
+    return array
+  }, A[0].split(''));
+};
