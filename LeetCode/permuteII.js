b0bd351ff292f8ea194f855c99fe3246cfc63bbd
Victor Tran <60512497+VictorTran-rc@users.noreply.github.com>
2020-05-24 22:06:34 -0400

    Add files via upload

---
+++ LeetCode/permuteII.js
@@ -0,0 +1,32 @@
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+Input: [1,1,2]
+Output:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+
+var permuteUnique = function(nums){
+  nums.sort(function(a,b){return a-b});
+  var used ={}, n=nums.length, res=[];
+  var recur = function(cur) {
+    if(cur.length == n){
+      res.push(cur.slice());
+    }
+
+    for(let = i; i<n; i++){
+      if(used[i] || (i>0 && nums[i-1]== nums[1] && !used[i-1])) {
+        continue;
+      }
+      cur.push(nums[i]);
+      used[i]=true;
+      recur(cur);
+      used[i]=false;
+      cur.pop();
+    }
+  }
+  recur([]);
+  return res;
+}
