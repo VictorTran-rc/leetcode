e3eafb0928aea4976e89916cb07ad177a6e0959c
Victor Tran <60512497+VictorTran-rc@users.noreply.github.com>
2020-06-02 21:57:10 -0400

    Update BestTimeToBuyAndSell.js

--- LeetCode/BestTimeToBuyAndSell.js
+++ LeetCode/BestTimeToBuyAndSell.js
@@ -3,7 +3,7 @@ Say you have an array prices for which the ith element is the price of a given s
 Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
 
 Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
-
+hi
 var maxProfit = function(prices){
    let profit = 0;
     for(let i = 1; i < prices.length; i++){
