3fd2dcdae4861fa2c7d55014d2702b374a86d4bf
Victor Tran <60512497+VictorTran-rc@users.noreply.github.com>
2020-07-12 21:51:22 -0400

    Update IsSubsequence.js

--- LeetCode/IsSubsequence.js
+++ LeetCode/IsSubsequence.js
@@ -1,14 +1,12 @@
 Given a string s and a string t, check if s is subsequence of t.
 
-A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a subsequence of "abcde" while "aec" is not).
+A subsequence of a string is a new string which is formed from the original string by deleting some
+(can be none) of the characters without disturbing the relative positions of the remaining characters.
+(ie, "ace" is a subsequence of "abcde" while "aec" is not).
 
 Follow up:
-If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if T has its subsequence. In this scenario, how would you change your code?
-
-Credits:
-Special thanks to @pbrother for adding this problem and creating all test cases.
-
-
+If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if T has its subsequence. 
+In this scenario, how would you change your code?
 
 Example 1:
 
@@ -20,10 +18,10 @@ Input: s = "axc", t = "ahbgdc"
 Output: false
 
 //1) check if s is an empty string
-//2) have two pointers to check the
+//2) have two pointers to check the array
 //3) while loop to check s and t variables
-//4) another while loop to
-//5)
+//4) another while loop to check if s === t 
+//5) 
 //6)
 
 let isSubsequence = function(s, t) {
