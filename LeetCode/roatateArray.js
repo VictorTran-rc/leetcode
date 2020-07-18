8cc9f71c6a0a93420b0131aebb699b1912ebf4f9
Victor Tran <60512497+VictorTran-rc@users.noreply.github.com>
2020-07-18 17:29:29 -0400

    Update roatateArray.js

--- LeetCode/roatateArray.js
+++ LeetCode/roatateArray.js
@@ -30,3 +30,35 @@ let rotate = function(nums, k) {
     k--;
   }
 };
+
+// reverse method
+// 1) set k to divide the value of k to the length of the array 
+// 2) reverse the array 
+// 3) reverse the first k element in the array 
+// 4) reverse the rest of the numbers 
+// 5) create a reverse function that will take in the whole array from start to end
+// 6) a while loop to from start to end
+// 7) have a variable to hold the first element 
+// 8) replace the first element with the last element 
+// 9) replace the last element with the first element 
+// 10) incremedemnt start by 1 
+// 11) decrement end by 1 
+
+
+var rotate = function(nums, k) {
+    k = k % nums.length; 
+    
+    reverse(nums, 0, nums.length -1);
+    reverse(nums, k, nums.length - 1); 
+    reverse(nums, 0, k-1 )
+};
+
+let reverse = function(nums, start, end){
+    while(start < end) {
+        let temp = nums[start]; 
+        nums[start] = nums[end];
+        nums[end] = temp; 
+        start++;
+        end--;       
+    }
+};
