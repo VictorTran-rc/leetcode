1e3c9d952e863ee3d83d119d9c74dacc8b63390b
Victor Tran <60512497+VictorTran-rc@users.noreply.github.com>
2020-07-21 12:44:34 -0400

    Update interectionOfTwoArrays.js

--- LeetCode/interectionOfTwoArrays.js
+++ LeetCode/interectionOfTwoArrays.js
@@ -17,3 +17,29 @@ Follow up:
 What if the given array is already sorted? How would you optimize your algorithm?
 What if nums1's size is small compared to nums2's size? Which algorithm is better?
 What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+
+  var intersect = function(nums1, nums2) {
+    const count = {};
+    
+    for (let i = 0; i < nums1.length; i++) {
+        const curr = nums1[i];
+        if (count[curr] == null) {
+            count[curr] = 1;
+        } else {
+            count[curr]++;
+        }
+    }
+    
+    
+    const result = [];
+
+    for (let i = 0; i < nums2.length; i++) {
+        const curr = nums2[i];
+        if (count[curr] > 0) {
+            result.push(curr);
+            count[curr]--;
+        }
+    }
+    
+    return result;
+};
