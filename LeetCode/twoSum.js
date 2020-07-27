4689ed01930ddb525bfea4ce3fad22b5d3b1b838
Victor Tran <60512497+VictorTran-rc@users.noreply.github.com>
2020-07-26 23:37:02 -0400

    Update twoSum.js

--- LeetCode/twoSum.js
+++ LeetCode/twoSum.js
@@ -8,11 +8,16 @@ Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
-1)
-2)
-3)
-4)
-5)
+
+// 1) Create map that holds the numbers and the indices- location of the element 
+// 2) Have an empty array to store the number and the indices
+// 3) loop through every element in the array 
+// 4) whatever variable that we are on. Save it to nums to keep track of where it is 
+// 5) Save the differences between the number you are on to the target 
+// 6) check if the number sorted in the array is in the map 
+// 7) have the complement number in the array be the index 
+
+
 var twoSum = function(nums, target) {
    const addition = {};
     for(let i=0; i<nums.length; i++){
